3267c7a6d80587be15617cd9dbd0c46ff425b477
jeremaihloo <1006397539@qq.com>
2015-07-17 16:22:14 +0800

    fore template simple finished

--- user/contact.php
+++ user/contact.php
@@ -18,20 +18,56 @@
 
 
 
+<style>
+    .main{
+        padding: 30px;
+    }
+    .form-line{
+        margin-top: 10px;
+    }
+    #validate{
 
+    }
+    #validate div{
+        float: left;
+        margin-left: 10px;
+    }
+    .clear{
+        clear: both;
+    }
+</style>
 <form>
-    <div style="padding: 30px;"
-    <label for="name">姓名</label>
-    <input id="name" name="name" type="text" />
+    <div class="main">
+        <div class="form-line">
+            <label for="name">姓名</label>
+            <input id="name" name="name" type="text" />
+        </div>
+        <div class="form-line">
+            <label for="phone">手机号</label>
+            <input id="phone" name="phone" type="text" />
+        </div>
+        <div class="form-line" id="validate">
+            <div>
+                <label for="validatecode">验证码</label>
+            </div>
+            <div>
+                <input id="validatecode" name="validatecode" type="text"/>
+            </div>
+            <div>
+                <img title="点我刷新" src="/validate-code" onclick="this.src='/validate-code?'+Math.random();" />
+            </div>
 
-    <label for="phone">手机号</label>
-    <input id="phone" name="phone" type="text" />
-
-    <label for="validatecode">验证码</label>
-    <input id="validatecode" name="validatecode"/>
-    
-    <img title="点我刷新" src="/validate-code" onclick="this.src='/validate-code?'+Math.random();" />
-    <input type="submit" value="提交" />
+            <div class="clear"></div>
+        </div>
+        <div>
+            <br />
+            <br />
+            <br />
+        </div>
+        <div class="form-line">
+            <input type="submit" value="提交" />
+        </div>
+    </div>
 </form>
 
 
